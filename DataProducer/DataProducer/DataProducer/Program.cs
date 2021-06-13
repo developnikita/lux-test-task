f9525fa61307d87e6986ab067aeb00f9022aa225
Nikita Korolov <nikitakorolov6@gmail.com>
2021-06-13 15:09:53 +0600

    add read data from xlsx to console

--- DataProducer/DataProducer/DataProducer/Program.cs
+++ DataProducer/DataProducer/DataProducer/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace DataProducer
 {
@@ -6,7 +10,21 @@ namespace DataProducer
     {
         static void Main(string[] args)
         {
-            
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var sb = new StringBuilder();
+            using (ExcelPackage excelPackage = new ExcelPackage(new FileInfo(@"C:\Users\Nekit\Desktop\SpeedAndPump.xlsx")))
+            {
+                var worksheet = excelPackage.Workbook.Worksheets.First();
+                var totalRows = worksheet.Dimension.End.Row;
+                var totalColumns = worksheet.Dimension.End.Column;
+
+                for (int rowNum = 1; rowNum <= totalRows; ++rowNum)
+                {
+                    var row = worksheet.Cells[rowNum, 1, rowNum, totalColumns].Select(c => c.Value == null ? string.Empty : c.Value.ToString());
+                    sb.AppendLine(string.Join(",", row));
+                }
+            }
+            Console.WriteLine(sb.ToString());
         }
     }
 }
