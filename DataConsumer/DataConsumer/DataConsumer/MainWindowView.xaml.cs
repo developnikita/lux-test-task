6f2517cb9d7aa648ac0bca9d127faf4f654b9aa9
Nikita Korolov <nikitakorolov6@gmail.com>
2021-06-14 20:35:44 +0600

    add connect to mqtt broker

---
+++ DataConsumer/DataConsumer/DataConsumer/MainWindowView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace DataConsumer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+    }
+}
---
+++ DataConsumer/DataConsumer/DataConsumer/MainWindowViewModel.cs
@@ -0,0 +1,100 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Receiving;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DataConsumer
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel()
+        {
+            _text = string.Empty;
+            var factory = new MqttFactory();
+            var options = new MqttClientOptionsBuilder()
+                .WithTls(new MqttClientOptionsBuilderTlsParameters()
+                {
+                    UseTls = false,
+                    IgnoreCertificateChainErrors = true,
+                    IgnoreCertificateRevocationErrors = true,
+                    AllowUntrustedCertificates = true,
+                })
+                .WithClientId(Guid.NewGuid().ToString())
+                .WithTcpServer("localhost", 1883)
+                .WithCredentials("admin", "admin")
+                .WithCleanSession(true)
+                .WithKeepAlivePeriod(TimeSpan.FromSeconds(5))
+                .Build();
+
+            _client = factory.CreateMqttClient();
+            _client.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnSubscriberConnected);
+            _client.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnSubscriberDisconnected);
+            _client.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(OnSubscriberMessageReceived);
+
+            Task.Run(() => _client.ConnectAsync(options)).Wait();
+
+
+            Task.Run(() =>
+            {
+                var topicFilter = new MqttTopicFilterBuilder()
+                    .WithTopic("MyTopic")
+                    .Build();
+                _client.SubscribeAsync(topicFilter);
+            }).Wait();
+        }
+
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+
+            set
+            {
+                if (_text == value)
+                    return;
+
+                _text = value;
+                RaisePropertyChanged(nameof(Text));
+            }
+        }
+
+        private void OnSubscriberMessageReceived(MqttApplicationMessageReceivedEventArgs x)
+        {
+            var item = $"Timestamp: {DateTime.Now:O} | Topic: {x.ApplicationMessage.Topic} | Payload: {x.ApplicationMessage.ConvertPayloadToString()} | QoS: {x.ApplicationMessage.QualityOfServiceLevel}";
+            Text = item;
+            // this.BeginInvoke((MethodInvoker)delegate { this.TextBoxSubscriber.Text = item + Environment.NewLine + this.TextBoxSubscriber.Text; });
+        }
+
+        private void OnSubscriberConnected(MqttClientConnectedEventArgs e)
+        {
+            MessageBox.Show("Subscriber connected", "ConnectHandler");
+        }
+
+        private void OnSubscriberDisconnected(MqttClientDisconnectedEventArgs e)
+        {
+            MessageBox.Show("Subscriber disconnected", "DisconnectHandler");
+        }
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private IMqttClient _client;
+        private string _text;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
