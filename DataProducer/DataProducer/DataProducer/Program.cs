580f98c19e4d61f21774697133ab892c8a488552
Nikita Korolov <nikitakorolov6@gmail.com>
2021-06-14 20:36:09 +0600

    add message publish to mqtt broker

--- DataProducer/DataProducer/DataProducer/Program.cs
+++ DataProducer/DataProducer/DataProducer/Program.cs
@@ -1,15 +1,44 @@
-﻿using OfficeOpenXml;
+﻿using MQTTnet;
+using MQTTnet.Client.Options;
+using OfficeOpenXml;
 using System;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DataProducer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var factory = new MqttFactory();
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer("localhost", 1883)
+                .WithCredentials("admin", "admin")
+                .WithClientId(Guid.NewGuid().ToString())
+                .Build();
+
+            var client = factory.CreateMqttClient();
+            var result = await client.ConnectAsync(options, CancellationToken.None);
+
+            if (client.IsConnected)
+                Console.WriteLine("YES");
+
+            while (true)
+            {
+                var message = new MqttApplicationMessageBuilder()
+                            .WithTopic("MyTopic")
+                            .WithPayload("Hello World")
+                            .WithQualityOfServiceLevel(1)
+                            .WithRetainFlag(true)
+                            .Build();
+                await client.PublishAsync(message, CancellationToken.None);
+                Thread.Sleep(1000);
+            }
+
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
             var sb = new StringBuilder();
             using (ExcelPackage excelPackage = new ExcelPackage(new FileInfo(@"C:\Users\Nekit\Desktop\SpeedAndPump.xlsx")))
@@ -24,7 +53,7 @@ namespace DataProducer
                     sb.AppendLine(string.Join(",", row));
                 }
             }
-            Console.WriteLine(sb.ToString());
+            // Console.WriteLine(sb.ToString());
         }
     }
 }
